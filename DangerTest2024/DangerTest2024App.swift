b0432552faae6e90454fc968e8bfe1238516a73d
Curtis Scott <curtis.scott@gocity.com>
2024-02-05 15:47:21 +0000

    Initial Commit

---
+++ DangerTest2024/DangerTest2024App.swift
@@ -0,0 +1,17 @@
+//
+//  DangerTest2024App.swift
+//  DangerTest2024
+//
+//  Created by Curtis Scott on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct DangerTest2024App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
