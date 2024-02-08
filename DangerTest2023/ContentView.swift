7bb29e2d31b0e7095aa2ceeb8d85078533fdad01
Curtis Scott <curtis.scott@gocity.com>
2024-02-08 19:53:42 +0000

    change

--- DangerTest2023/ContentView.swift
+++ DangerTest2023/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world! again ")
+            Text("Hello, world! again this is changed again  ")
         }
         .padding()
     }
